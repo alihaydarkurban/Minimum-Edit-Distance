935dc8821902d53a973b0ea0d4775d0675a4050f
alihaydarkurban <a.haydarkurban@gmail.com>
2020-11-12 15:55:21 +0300

    Updated

--- min_edit_distance.py
+++ min_edit_distance.py
@@ -7,12 +7,72 @@ def read_random_file():
         if len(line) > 15:
             line = line[: 15]
         words.append(line)
-    file_handle.close()
 
+    file_handle.close()
     return words
 
 
+def get_word():
+    word = input("Enter main word : ")
+
+    if not word:
+        print("You must enter non-empty word!\nProgram is terminating...")
+        exit()
+
+    elif len(word) > 15:
+        word = word[: 15]
+
+    return word
+
+
+def min_edit_distance(word_1, word_2):
+    print("main word :", word_1, "with length : ", len(word_1))
+    print("other word :", word_2, "with length : ", len(word_2))
+
+    len_word_1 = len(word_1)  # vertical word |
+    len_word_2 = len(word_2)  # horizontal word -
+
+    table = [[0 for i in range(len_word_2 + 1)] for i in range(len_word_1 + 1)]
+    table_initialization(table, len_word_1, len_word_2)
+
+    for i in range(1, len_word_1 + 1):
+        for j in range(1, len_word_2 + 1):
+            if word_1[i - 1] == word_2[j - 1]:
+                table[i][j] = table[i - 1][j - 1]
+                substitution_cost = 0 + table[i - 1][j - 1]
+            else:
+                substitution_cost = 2 + table[i - 1][j - 1]
+
+            insert_or_delete_cost = min(table[i][j - 1], table[i - 1][j]) + 1
+
+            table[i][j] = min(substitution_cost, insert_or_delete_cost)
+
+    print_table(table)
+
+
+def table_initialization(table, len_word_1, len_word_2):
+    for i in range(len_word_2 + 1):
+        table[0][i] = i
+    for j in range(len_word_1 + 1):
+        table[j][0] = j
+
+
+def print_table(table):
+    row = len(table)
+    column = len(table[0])
+    print("row :", row)
+    print("column :", column)
+
+    for i in range(row):
+        print(table[i])
+
+
 if __name__ == '__main__':
-    random_words = read_random_file()
-    print(random_words)
-    print(len(random_words))
+    # random_words = read_random_file()
+
+    main_word = get_word()
+    other_word = get_word()
+    min_edit_distance(main_word, other_word)
+
+    # print(random_words)
+    # print(len(random_words))
