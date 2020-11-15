181259e001ce2428ae0a9a5784d3f9fad734361d
alihaydarkurban <a.haydarkurban@gmail.com>
2020-11-16 00:32:15 +0300

    3D Table Implementation Updated

--- min_edit_distance.py
+++ min_edit_distance.py
@@ -1,9 +1,10 @@
 MAX_LENGTH = 15
 TOP_FIVE = 5
+EXPENSIVE_WORD = -1
 
 
 class MostSimilarWord:
-    def __init__(self, index=-1, word="?", cost=30):
+    def __init__(self, index=-1, word="?", cost=100):
         self.index = index
         self.word = word
         self.cost = cost
@@ -17,7 +18,7 @@ class MostSimilarWord:
 
 
 def read_random_file():
-    file_handle = open("Files/test_file.txt", "r", encoding='utf8')
+    file_handle = open("Files/random_file_6.txt", "r", encoding='utf8')
     read_line = file_handle.readlines()
     words = []
     for line in read_line:
@@ -45,19 +46,25 @@ def get_word():
 
 def min_edit_distance(word_1, words_of_file, top_5_words):
     number_of_words = len(words_of_file)
-    table = [[[0 for x in range(MAX_LENGTH + 1)] for y in range(MAX_LENGTH + 1)] for z in range(number_of_words)]
+    table = [[[None for x in range(MAX_LENGTH + 1)] for y in range(MAX_LENGTH + 1)] for z in range(number_of_words)]
 
     len_word_1 = len(word_1)  # vertical word |
     len_word_2 = 0  # horizontal word -
 
     table_initialization(table, number_of_words)
 
-    for k in range(number_of_words):
-        word_2 = words_of_file[k]
-        len_word_2 = len(word_2)
+    # i (1,16) # It represents the main word
+    #     k (1000) # It is the total numbers of table
+    #         j (1,16) # It represents the word is in the file
 
-        for i in range(1, len_word_1 + 1):
+    for i in range(1, len_word_1 + 1):
+        for k in range(number_of_words):
+            word_2 = words_of_file[k]
+            len_word_2 = len(word_2)
             for j in range(1, len_word_2 + 1):
+                if table[k][0][0] < 0:
+                    break
+
                 if word_1[i - 1] == word_2[j - 1]:
                     substitution_cost = 0
                 else:
@@ -66,21 +73,58 @@ def min_edit_distance(word_1, words_of_file, top_5_words):
                 table[k][i][j] = min(table[k][i][j - 1] + 1, table[k][i - 1][j] + 1,
                                      table[k][i - 1][j - 1] + substitution_cost)
 
-        cost = table[k][i][j]
-        if cost < top_5_words[TOP_FIVE - 1].cost:
-            top_5_words.pop()
-            new_word = MostSimilarWord(k, word_2, cost)
-            top_5_words.append(new_word)
+                if j == len_word_2:
+                    top_5_words = sorted(top_5_words, key=lambda x: x.cost)
+                    can_min = abs(table[k][i][j] - (len_word_1 - i))
+                    can_max = abs(table[k][i][j] + (len_word_1 - i))
 
-        top_5_words = sorted(top_5_words, key=lambda x: x.cost)
+                    appendable = True
+                    if can_max < top_5_words[TOP_FIVE - 1].cost:
+                        new_word = MostSimilarWord(k, word_2, can_max)
 
-        print("Total Cost :", cost)
-        print("main word :", word_1)
-        print("other word :", word_2)
-        print_table(table[k])
+                        for msw in top_5_words:
+                            if msw.index == new_word.index:
+                                appendable = False
+                                if msw.cost != new_word.cost:
+                                    msw.cost = new_word.cost
+
+                        if appendable:
+                            top_5_words.pop()
+                            top_5_words.append(new_word)
+
+                    if can_min > top_5_words[TOP_FIVE - 1].cost:
+                        table[k][0][0] = EXPENSIVE_WORD  # EXPENSIVE_WORD = -1
+
+    for i in range(number_of_words):
+        print_table(table[i])
+        print("INDEX :", i)
+        print("Main Word :", word_1)
+        print("Other Word :", words_of_file[i])
+        if table[i][0][0] != -1:
+            print("================================================="
+                  "==========================="
+                  "===================================COST :", table[i][len_word_1][len(words_of_file[i])])
         print("*******************************************************")
 
-    return top_5_words
+    for i in range(TOP_FIVE):
+        top_5_words[i].print_msw()
+
+    calculate_percentage(table, number_of_words)
+
+
+def calculate_percentage(table, x):
+
+    num_of_None = 0
+    for k in range(x):
+        for i in range(MAX_LENGTH + 1):
+            for j in range(MAX_LENGTH + 1):
+                if table[k][i][j] is None:
+                    num_of_None = num_of_None + 1
+
+    total_usage = ((MAX_LENGTH + 1) * (MAX_LENGTH + 1) * x) - num_of_None
+    percentage = (total_usage * 100.0) / ((MAX_LENGTH + 1) * (MAX_LENGTH + 1) * x)
+
+    print("PERCENTAGE : ", percentage)
 
 
 def table_initialization(table, word_of_file_count):
@@ -108,7 +152,4 @@ if __name__ == '__main__':
     # print(len(random_words))
 
     most_similar_five_words = [MostSimilarWord() for i in range(TOP_FIVE)]
-    most_similar_five_words = min_edit_distance("ali", random_words, most_similar_five_words)
-
-    for i in range(TOP_FIVE):
-        most_similar_five_words[i].print_msw()
+    min_edit_distance("ali", random_words, most_similar_five_words)
